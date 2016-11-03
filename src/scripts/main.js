a67556f81c4a45094e2bc189825d6a9807046968
Dan Allen <dan.j.allen@gmail.com>
2016-11-02 20:45:47 -0600

    swap onstage for new presentation console; disable bullets plugin

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,10 @@
 var bespoke = require('bespoke'),
-  bullets = require('bespoke-bullets'),
+  //bullets = require('bespoke-bullets'),
   classes = require('bespoke-classes'),
   extern = require('bespoke-extern'),
   hash = require('bespoke-hash'),
   multimedia = require('bespoke-multimedia'),
   nav = require('bespoke-nav'),
-  onstage = require('bespoke-onstage'),
   overview = require('bespoke-overview'),
   scale = require('bespoke-scale');
 
@@ -14,9 +13,67 @@ bespoke.from({ parent: 'article.deck', slides: 'section' }, [
   scale(),
   nav(),
   overview(),
-  bullets('.build, .build-items > *:not(.build-items)'),
-  onstage(),
+  //bullets('.build, .build-items > *:not(.build-items)'),
   hash(),
   multimedia(),
+  function (deck) {
+    const metas = {}
+    Array.from(document.querySelectorAll('head meta')).forEach((meta) => {
+        metas[meta.getAttribute('name')] = meta.getAttribute('content')
+    })
+
+    const steps = deck.slides.map((slide, slideIdx) => {
+
+      const notes = [].slice.call(slide.querySelectorAll('aside[role="note"] p, aside[role="note"] li'))
+        .map((note) => note.textContent)
+        .join('\n')
+
+      //if (slide.bullets.length > 0) {
+      //  return slide.bullets.map((b, bulletIdx) => {
+      //    return {
+      //      cursor: String(slideIdx) + '.' + String(bulletIdx),
+      //      states: [],
+      //      notes,
+      //    }
+      //  })
+      //}
+
+      return {
+        cursor: String(slideIdx),
+        states: [],
+        notes
+      }
+    })
+
+    const details = {
+      title: document.title || '',
+      authors: metas.author || '',
+      description: metas.description || '',
+      vendor: 'bespoke.js',
+      steps,
+      ratios: ['16/9'],
+      themes: ['default']
+    }
+
+    window.addEventListener('message', ({ source, data: { command, commandArgs } }) => {
+
+      switch (command) {
+
+        case 'get-slide-deck-details':
+          source.postMessage({ event: 'slide-deck-details', eventData: { details } }, '*')
+          break;
+
+        case 'go-to-step':
+          const { cursor } = commandArgs
+          const [slideIdx, subslideIdx] = cursor.split('.')
+          deck.slide(Number(slideIdx))
+          deck.activateBullet(Number(slideIdx), Number(subslideIdx))
+          break;
+
+        default:
+          console.debug(`unknown protocol command ${command} with args`, commandArgs)
+      }
+    })
+  },
   extern(bespoke)
 ]);
